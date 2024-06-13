17d24a6d46e094c7f1b610943697b616743999ea
Omar-NF <a21110112@ceti.mx>
2024-06-13 15:26:59 -0600

    tercer commit

--- app/src/main/java/com/example/proyectofinal/Admin_Agregar_Activity.kt
+++ app/src/main/java/com/example/proyectofinal/Admin_Agregar_Activity.kt
@@ -5,14 +5,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Spinner
 
 class Admin_Agregar_Activity : AppCompatActivity() {
+    private lateinit var nombreLibros : EditText
+    private lateinit var cantidadLibros : Spinner
+    private lateinit var precio_Libros : EditText
+    private lateinit var grupo : RadioGroup
+    private lateinit var renta : RadioButton
+    private lateinit var venta : RadioButton
+    private lateinit var ambos : RadioButton
     private lateinit var guardar : Button
     private lateinit var borrar : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_agregar)
 
+        nombreLibros = findViewById(R.id.edtNombre)
+        cantidadLibros = findViewById(R.id.spCantidad)
+        precio_Libros = findViewById(R.id)
+        grupo = findViewById(R.id)
+        renta = findViewById(R.id)
+        venta = findViewById(R.id)
+        ambos = findViewById(R.id)
         guardar = findViewById(R.id.btnGuardarLib)
         borrar = findViewById(R.id.btnBorrarLib)
     }
--- app/src/main/java/com/example/proyectofinal/Libro.kt
+++ app/src/main/java/com/example/proyectofinal/Libro.kt
@@ -1,4 +1,8 @@
 package com.example.proyectofinal
 
 class Libro {
+    var nombre : String = ""
+    var cantidad : Int = 0
+    var precio : Int = 0
+    var disponibilidad :String = ""
 }
