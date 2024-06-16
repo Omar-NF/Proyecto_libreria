bfb4f7b602dc54a87dffb3aec1cdbc1238db92d8
JossAx525 <106834412+JossAx525@users.noreply.github.com>
2024-06-16 13:59:03 -0600

    Update Libro.kt

--- app/src/main/java/com/example/proyectofinal/Libro.kt
+++ app/src/main/java/com/example/proyectofinal/Libro.kt
@@ -2,8 +2,9 @@ package com.example.proyectofinal
 
 data class Libro(
     val id: String,
-    var nombre : String,
-    var genero : String,
-    var precio : Int,
-    var disponibilidad : String
+    var nombre: String,
+    var genero: String,
+    var precio: Int,
+    var disponibilidad: String,
+    var descripcion: String // Nuevo atributo
 )
