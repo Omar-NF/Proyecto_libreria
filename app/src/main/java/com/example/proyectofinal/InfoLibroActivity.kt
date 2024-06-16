1699c9bba3054e6cfd6d6bfd5e559d9594d379b4
JossAx525 <106834412+JossAx525@users.noreply.github.com>
2024-06-16 17:48:24 -0600

    Update and rename Info_Libro_Activity.kt to InfoLibroActivity.kt

--- app/src/main/java/com/example/proyectofinal/InfoLibroActivity.kt
+++ app/src/main/java/com/example/proyectofinal/InfoLibroActivity.kt
@@ -3,7 +3,7 @@ package com.example.proyectofinal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Info_Libro_Activity : AppCompatActivity() {
+class InfoLibroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info_libro)
