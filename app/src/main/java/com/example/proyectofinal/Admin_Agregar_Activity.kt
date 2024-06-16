18eb0a5cc2ef8b63f22605e65ecf1046adfaad24
JossAx525 <106834412+JossAx525@users.noreply.github.com>
2024-06-16 14:04:00 -0600

    Update Admin_Agregar_Activity.kt

--- app/src/main/java/com/example/proyectofinal/Admin_Agregar_Activity.kt
+++ app/src/main/java/com/example/proyectofinal/Admin_Agregar_Activity.kt
@@ -18,18 +18,20 @@ import com.google.gson.reflect.TypeToken
 import java.util.UUID
 
 class Admin_Agregar_Activity : AppCompatActivity() {
-    private lateinit var nombreLibros : EditText
-    private lateinit var genero_Libros : Spinner
-    private lateinit var precio_Libros : EditText
-    private lateinit var grupo : RadioGroup
-    private lateinit var renta : RadioButton
-    private lateinit var venta : RadioButton
-    private lateinit var ambos : RadioButton
-    private lateinit var guardar : Button
-    private lateinit var borrar : Button
+    private lateinit var nombreLibros: EditText
+    private lateinit var genero_Libros: Spinner
+    private lateinit var precio_Libros: EditText
+    private lateinit var descripcion_Libros: EditText // Nuevo campo
+    private lateinit var grupo: RadioGroup
+    private lateinit var renta: RadioButton
+    private lateinit var venta: RadioButton
+    private lateinit var ambos: RadioButton
+    private lateinit var guardar: Button
+    private lateinit var borrar: Button
     private var opcionSel = "1"
     private val sharedPrefsFile = "LibraryPrefs"
     private val gson = Gson()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_agregar)
@@ -37,6 +39,7 @@ class Admin_Agregar_Activity : AppCompatActivity() {
         nombreLibros = findViewById(R.id.edtNombre)
         genero_Libros = findViewById(R.id.spSpinner)
         precio_Libros = findViewById(R.id.edtPrecio)
+        descripcion_Libros = findViewById(R.id.edtDescripcion) // Nuevo campo
         grupo = findViewById(R.id.rdGrupo)
         renta = findViewById(R.id.rdRenta)
         venta = findViewById(R.id.rdVenta)
@@ -47,32 +50,22 @@ class Admin_Agregar_Activity : AppCompatActivity() {
         //---------------------------------- Spinner------------------------------------------
         val lstGeneros = resources.getStringArray(R.array.stArrayGeneros)
         val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, lstGeneros)
-        //asosiacion
         genero_Libros.adapter = adaptador
-        //Oidor de eventos
-        genero_Libros.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                opcionSel = lstGeneros [position]
+        genero_Libros.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                opcionSel = lstGeneros[position]
             }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
-        }//oidor
-    }//onCreate
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+    }
 
     fun guardarLibro(view: View) {
-        if (nombreLibros.text.isNotBlank() && nombreLibros.text.isNotEmpty() &&
-            precio_Libros.text.isNotBlank() && precio_Libros.text.isNotEmpty()) {
-
+        if (nombreLibros.text.isNotBlank() && precio_Libros.text.isNotBlank() && descripcion_Libros.text.isNotBlank()) {
             val nombre = nombreLibros.text.toString()
             val genero = opcionSel
             val precio = precio_Libros.text.toString().toInt()
+            val descripcion = descripcion_Libros.text.toString() // Nuevo campo
             var disponibilidad = ""
             if (renta.isChecked) disponibilidad = "Renta"
             if (venta.isChecked) disponibilidad = "Venta"
@@ -89,13 +82,12 @@ class Admin_Agregar_Activity : AppCompatActivity() {
 
             val libroExistente = libros.find { it.nombre == nombre }
             if (libroExistente != null) {
-                // Actualizamos el libro existente
                 libroExistente.genero = genero
                 libroExistente.precio = precio
                 libroExistente.disponibilidad = disponibilidad
+                libroExistente.descripcion = descripcion // Nuevo campo
             } else {
-                // Creamos un nuevo libro
-                val libro = Libro(UUID.randomUUID().toString(), nombre, genero, precio, disponibilidad)
+                val libro = Libro(UUID.randomUUID().toString(), nombre, genero, precio, disponibilidad, descripcion)
                 libros.add(libro)
             }
 
@@ -107,39 +99,15 @@ class Admin_Agregar_Activity : AppCompatActivity() {
         }
     }
 
-
-    fun guardarShared(libro: Libro){
-        // 1. Obtener una instancia de SharedPreferences
-        val sharedPreferences = getSharedPreferences(sharedPrefsFile, Context.MODE_PRIVATE)
-        // 2. Crear un editor para modificar SharedPreferences
-        val editor = sharedPreferences.edit()
-        // 3. Recuperar la lista actual de libros en formato JSON
-        val json = sharedPreferences.getString("libros", null)
-        // 4. Definir el tipo de la lista de libros
-        val type = object : TypeToken<MutableList<Libro>>() {}.type
-        // 5. Deserializar el JSON a una lista mutable de objetos Libro si existe, o crear una lista vacía si no
-        val libros: MutableList<Libro> = if (json != null) {
-            gson.fromJson(json, type)
-        } else {
-            mutableListOf()
-        }
-        // 6. Agregar el nuevo libro a la lista
-        libros.add(libro)
-        // 7. Serializar la lista actualizada de libros a formato JSON
-        val jsonString = gson.toJson(libros)
-        // 8. Guardar la lista actualizada de libros en SharedPreferences
-        editor.putString("libros", jsonString)
-        editor.apply()
-    }
-
     private fun limpiar() {
         nombreLibros.text = null
         precio_Libros.text = null
+        descripcion_Libros.text = null // Nuevo campo
         grupo.clearCheck()
     }
 
-    fun regresarAdmin(view: View){
-        val intent = Intent(this,AdminActivity::class.java)
+    fun regresarAdmin(view: View) {
+        val intent = Intent(this, AdminActivity::class.java)
         startActivity(intent)
     }
 
@@ -157,9 +125,9 @@ class Admin_Agregar_Activity : AppCompatActivity() {
 
             val libro = libros.find { it.nombre == nombre }
             if (libro != null) {
-                // Llenar los campos de edición con los datos del libro
                 nombreLibros.setText(libro.nombre)
                 precio_Libros.setText(libro.precio.toString())
+                descripcion_Libros.setText(libro.descripcion) // Nuevo campo
                 when (libro.disponibilidad) {
                     "Renta" -> renta.isChecked = true
                     "Venta" -> venta.isChecked = true
@@ -173,7 +141,4 @@ class Admin_Agregar_Activity : AppCompatActivity() {
             Toast.makeText(this, "Ingresa el nombre del libro", Toast.LENGTH_LONG).show()
         }
     }
-
-
-
 }
