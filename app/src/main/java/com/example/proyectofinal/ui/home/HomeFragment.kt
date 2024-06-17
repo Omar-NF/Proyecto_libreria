4d17eb8cc893d901d7d3e926602ab0156cb7237b
JossAx525 <106834412+JossAx525@users.noreply.github.com>
2024-06-16 18:44:30 -0600

    Update HomeFragment.kt

--- app/src/main/java/com/example/proyectofinal/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/proyectofinal/ui/home/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.proyectofinal.databinding.FragmentHomeBinding
@@ -29,13 +28,6 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-}
     }
 
     override fun onDestroyView() {
