702b382f2c00f6a66695f8fb052750af48183111
JossAx525 <106834412+JossAx525@users.noreply.github.com>
2024-06-16 18:46:32 -0600

    Update InfoLibroFragment.kt

--- app/src/main/java/com/example/proyectofinal/InfoLibroFragment.kt
+++ app/src/main/java/com/example/proyectofinal/InfoLibroFragment.kt
@@ -3,7 +3,7 @@ package com.example.proyectofinal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class InfoLibroActivity : AppCompatActivity() {
+class InfoLibroFragment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info_libro)
